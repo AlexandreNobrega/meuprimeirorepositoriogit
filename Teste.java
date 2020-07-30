4b14559dc149aaba5d7cf51d5fa685b4210d035b
Alexandre <alexandrenobrega777@gmail.com>
2020-07-30 10:53:20 -0300

    'TESTE COMMIT'

---
+++ Teste.java
@@ -0,0 +1,18 @@
+public class Teste {
+
+    public static void main(String[] args){
+
+        System.out.println("Olá Mundo!");
+
+        System.out.println("Olá Mundo!");
+
+        System.out.println("Olá Mundo!");
+
+        System.out.println("Olá Mundo!");
+
+        System.out.println("git diff --staged");
+
+        System.out.println("git diff");
+
+    }
+}

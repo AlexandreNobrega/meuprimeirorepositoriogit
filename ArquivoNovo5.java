8c88cf968172db9d91f35913f60f8bcd661d915e
Alexandre <alexandrenobrega777@gmail.com>
2020-09-15 16:22:00 -0300

    'TESTE COMMIT 14/09'

---
+++ ArquivoNovo5.java
@@ -0,0 +1,10 @@
+Class ArquivoNovo5 {
+    public static void main(String[] args) {
+        
+        System.out.println("ARQUIVO NOVO 5");
+        System.out.println("ARQUIVO NOVO 5");
+        System.out.println("ARQUIVO NOVO 5");
+        System.out.println("ARQUIVO NOVO 5");
+
+    }
+}

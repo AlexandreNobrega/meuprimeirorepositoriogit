e521bebe31d0aedbea7f4aad806d2da59dab604d
Alexandre <alexandrenobrega777@gmail.com>
2020-09-16 10:43:08 -0300

    'TESTANDO O COMANDO --amend AGORA RENOMEANDO ELE NOVAMENTE'

--- ArquivoNovo6.java
+++ ArquivoNovo6.java
@@ -2,8 +2,7 @@
 Class ArquivoNovo6 {
     public static void main(String[] args) {
         
-        System.out.println("ARQUIVO NOVO 5");
-        System.out.println("ARQUIVO NOVO 5");
+        System.out.println("TESTANDO O --amend");
 
 
     }

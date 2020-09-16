334ef3f1bea55b60cb7c8262281be2a524ece657
Alexandre <alexandrenobrega777@gmail.com>
2020-09-16 10:32:14 -0300

    'TESTE ALTERAÇÃO ArquivoNovo6'

--- ArquivoNovo3.java
+++
@@ -1,9 +0,0 @@
-public class ArquiNovo3 {
-
-    public static void main(String[] args) {
-        
-        String nome = "Alexandre";
-
-    }
-
-}
--- ArquivoNovo6.java
+++ ArquivoNovo6.java
@@ -3,6 +3,8 @@ Class ArquivoNovo6 {
     public static void main(String[] args) {
         
         System.out.println("ARQUIVO NOVO 5");
+        System.out.println("ARQUIVO NOVO 5");
+
 
     }
 }

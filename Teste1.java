f2b3c9c31d4826d2ccb4c0a808130f9c1bb7484a
Alexandre <alexandrenobrega777@gmail.com>
2020-10-15 11:17:13 -0300

    made a change

--- Teste1.java
+++ Teste1.java
@@ -2,5 +2,6 @@ public class Teste1 {
     public static void main(String[] args) {
         System.out.println("Teste 1");
         System.out.println("Teste 1");
+        System.out.println("Teste 1");
     }
 }
